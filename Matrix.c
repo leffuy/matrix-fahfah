a9c5b9c7768c8bc631b72cf8871dd69e72b8ab62
Stevie Frederick <leflor@gmail.com>
2010-11-02 14:31:01 -0400

    Added working comments for Matrix structures. Push code into header file.

--- Matrix.c
+++ Matrix.c
@@ -1,3 +1,4 @@
+//this would make a good header file rearrange
 #include <stdio.h>
 #include <malloc.h>
 #include <math.h>
@@ -10,32 +11,41 @@ int col;
 float* data;
 }Matrix;
 
-//Interface for basic matrix operations
 /*typedef struct{
 int (*allocMatrix)(Matrix m) = 0;
 int (*freeMatrix)(int mNum) = 0;
 float (*MatrixSum)(int mNum) = 0;
-
 }MatFace;
 */
+/*
+The above was an experiment for polymorphism. Unnecessary for a traditional 
+interface. In following elements are stats of the "machine" operation
+as a whole.
 
+int mats;
+int* rs;
+int* cs;
+int* sid;
+float* matData;
+*/
+//Interface for basic matrix operations
 //This is a matrix machine posed as an interface.
 typedef struct{
 int (*initMachine)(int mdsz);
 int (*allocMatrix)(Matrix m);
 int (*freeMatrix)(int mNum);
 Matrix (*getMatrix)(int mNum);
-int mats;
-int* rs;
-int* cs;
-int* sid;
-float* matData;
 }MatMachine;
 
+//
 extern MatMachine* createMatMach(int version); //restrict with #define VERSION
 
-//Here are some comments to explain the beginning of this code
 
+
+//
+//End Useful Code
+//Below is just a test comment out or manipulate to sandbox
+//Here are some comments to explain the beginning of this code
 //a test main function wit
 int main(int argc, char** argv){
 //Begin Matrix Build
