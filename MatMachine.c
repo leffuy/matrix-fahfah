d6600caf5358a92437ebd6e62c09c00e2d049bde
Stevie Frederick <leflor@gmail.com>
2010-11-02 18:15:16 -0400

    This machine works with Matrix.h to implement the Matrix interface.

--- MatMachine.c
+++ MatMachine.c
@@ -4,16 +4,33 @@
 #include <math.h>
 #include <stdlib.h>
 
+MatMachine* g_mach;
 float* megaMatrix; 
 
-
+//Init the machine resources
 int initMachine(int mdsz){
 megaMatrix = (float*)malloc(sizeof(float)*mdsz);
 
 //Test the machine data structure return 'OK' if structure works
-megaMatrix[0] = 23.42
-if(megaMatrix[0] == 23.42)
+megaMatrix[0] = 23.0;
+
+if(megaMatrix[0] == 23.0)
 	return 0;
 
 return 1;
 }
+
+//Releases the machine resources. Call before using free()
+int releaseMachine(void){
+free(megaMatrix);
+return 0;
+}
+
+MatMachine* createMatMach(int version){
+g_mach = (MatMachine*)malloc(sizeof(MatMachine));
+g_mach->initMachine = &initMachine;
+g_mach->releaseMachine = &releaseMachine;
+return g_mach;
+}
+
+
