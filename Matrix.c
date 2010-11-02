8d314e6406409384d9fba075b74e1c348a7e87dd
Stevie Frederick <leflor@gmail.com>
2010-11-02 02:42:41 -0400

    Move to CPP classes will make sets easier.

--- Matrix.c
+++ Matrix.c
@@ -21,33 +21,32 @@ float (*MatrixSum)(int mNum) = 0;
 
 //the machine the implements the above interface
 typedef struct{
+int (*initMachine)(int mdsz);
+int (*allocMatrix)(Matrix m);
+int (*freeMatrix)(int mNum);
+Matrix (*getMatrix)(int mNum);
 int mats;
 int* rs;
 int* cs;
 int* sid;
 float* matData;
-int (*allocMatrix)(Matrix m);
-int (*freeMatrix)(int mNum);
-Matrix (*getMatrix)(int mNum);`
 }MatMachine;
 
 //Here are some comments to explain the beginning of this code
 
 //a test main function wit
 int main(int argc, char** argv){
+//Begin Matrix Build
 //Example of how to setup the above matrix typedef.
 srand(1342);
-
 //Establish the matrix
 Matrix bigM;
 //test variable; set to arbritary but static values
 int i = 0;
 int row = 3;
 int col = 3;
-
 //this will result in a size with some factor of nine 
 float* rdta = (float*)malloc(sizeof(float)*row*col);
-
 //setup the matrix
 bigM.row = row;
 bigM.col = col;
@@ -56,7 +55,11 @@ for(i=0; i<row*col; i++){
 }
 bigM.data = rdta;
 //After this you have a bigM that is "operational"
+//Ene Matrix Build
 
+
+//Test sandbox code
+//Here:
 
 printf("Element 4: %f \n", *((bigM.data)+3));
 printf("Free memory\n");
